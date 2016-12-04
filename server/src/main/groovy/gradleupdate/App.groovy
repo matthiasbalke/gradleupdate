00b8bc03d91c0b975cb6c1b490a3192e4eb66dbd
Hidetake Iwata <int128@gmail.com>
2016-12-04 23:21:18 +0900

    Use jetty and remove unused deps

--- server/src/main/groovy/gradleupdate/App.groovy
+++ server/src/main/groovy/gradleupdate/App.groovy
@@ -2,11 +2,7 @@ package gradleupdate
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.netflix.feign.EnableFeignClients
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client
 
-@EnableFeignClients
-@EnableOAuth2Client
 @SpringBootApplication
 class App {
     static void main(String[] args) {
