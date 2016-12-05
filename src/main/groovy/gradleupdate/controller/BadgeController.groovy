f8ccf2f8a1e4c703669f4d4cc88c2ca4d4d0fda0
Hidetake Iwata <int128@gmail.com>
2016-12-05 09:25:16 +0900

    WIP: Implement reading Gradle version from repo

--- src/main/groovy/gradleupdate/controller/BadgeController.groovy
+++ src/main/groovy/gradleupdate/controller/BadgeController.groovy
@@ -1,16 +1,44 @@
 package gradleupdate.controller
 
 import groovy.xml.MarkupBuilder
+import org.kohsuke.github.GitHub
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BadgeController {
+    // TODO: extract domain class
+    @Value('#{environment.GRADLEUPDATE_GITHUB_TOKEN}')
+    private String githubToken
+
     @GetMapping(value = '/{owner}/{repo}/status.svg', produces = 'image/svg+xml')
     String status(@PathVariable String owner, @PathVariable String repo) {
+        // TODO: extract domain class
+        println githubToken
+        def gitHub = GitHub.connectUsingOAuth(githubToken)
+        def repository = gitHub.getRepository("$owner/$repo")
+        def gradleWrapperProperties = repository.getFileContent('gradle/wrapper/gradle-wrapper.properties').read().text
+        def version = parseGradleWrapperProperties(gradleWrapperProperties)
+
         // TODO
-        render('unknown', '#9f9f9f')
+        render(version, '#9f9f9f')
+    }
+
+    // TODO: extract domain class
+    static String parseGradleWrapperProperties(String gradleWrapperProperties) {
+        assert gradleWrapperProperties
+        try {
+            def m = gradleWrapperProperties =~ /distributionUrl=.+?\/gradle-(.+?)-.+?\.zip/
+            assert m
+            def m0 = m[0]
+            assert m0 instanceof List
+            assert m0.size() == 2
+            m0[1] as String
+        } catch (AssertionError ignore) {
+            null
+        }
     }
 
     private static render(String message, String fill) {
